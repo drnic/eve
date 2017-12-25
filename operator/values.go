2886da5a3541df5d50597270b104c46ac9e15923
Dr Nic Williams <drnicwilliams@gmail.com>
2017-12-26 08:37:55 +1000

    values cmd can read from stdin if --target missing or '-'

--- operator/values.go
+++ operator/values.go
@@ -2,13 +2,19 @@ package operator
 
 import (
 	"io/ioutil"
+	"os"
 
 	"gopkg.in/yaml.v2"
 )
 
 // LoadValues loads values from Target file or STDIN
 func (out *Output) LoadValues() (err error) {
-	data, err := ioutil.ReadFile(out.TargetPath)
+	var data []byte
+	if out.TargetPath == "" || out.TargetPath == "-" {
+		data, err = ioutil.ReadAll(os.Stdin)
+	} else {
+		data, err = ioutil.ReadFile(out.TargetPath)
+	}
 	if err != nil {
 		return
 	}
